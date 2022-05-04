5f38d38b11eb18d13dd17a50774ebf1d83b84b85
Bbayro <brianbayrocuadros@gmail.com>
2022-05-03 20:38:19 -0500

    redis

--- src/main/java/com/example/personal/controller/PersonalController.java
+++ src/main/java/com/example/personal/controller/PersonalController.java
@@ -1,30 +1,43 @@
 package com.example.personal.controller;
 
-import com.example.personal.service.SequenceGeneratorService;
 import com.example.personal.entity.Personal;
 import com.example.personal.repository.PersonalRepository;
+import com.example.personal.service.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/personal")
 public class PersonalController {
 
     @Autowired
     private PersonalRepository personalRepository;
+
     @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
+
     @PostMapping
     public Personal save(@RequestBody Personal personal) {
         personal.setId(sequenceGeneratorService.generateSequence(Personal.SEQUENCE_NAME));
-        return personalRepository.save(personal);    }
+        return personalRepository.save(personal);
+    }
+
+    @Cacheable("personal")
     @GetMapping("/{dni}")
-    public Personal find(@PathVariable String dni) {
+    public Personal find(@PathVariable String dni) throws InterruptedException {
+
+        // Solo para pruebas de cache
+        Thread.sleep(1000);
+
         return personalRepository.findByDni(dni);
     }
+
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
         personalRepository.deleteById(id);
     }
+
     @PutMapping("/{id}")
     public Personal update(@PathVariable long id, @RequestBody Personal personal) {
         return personalRepository.save(personal);
