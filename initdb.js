9ae3d52b28925c22799b0928c0f61fc3168bb621
rbbayroc <brianbayrocuadros@gmail.com>
2022-05-04 06:54:43 -0500

    04-05.7

--- initdb.js
+++ initdb.js
@@ -1,5 +1,5 @@
 db.createCollection('Personal');
-db.Personal.insert({
+db.Personal.insert([{
    "_id":1,
    "name":"Bayro",
    "lastname":"Cuadros",
@@ -7,5 +7,53 @@ db.Personal.insert({
    "dni":"12345678",
    "phone":"123456789",
    "clienttype":"Personal",
-   "seq":1
-});
+   "_class": "com.example.personal.entity.Personal"
+},
+{
+  "_id": 2,
+  "name": "Brian",
+  "lastname": "Bayro Cuadros",
+  "email": "bbayro@gmail.com",
+  "dni": "45699429",
+  "phone": 999907132,
+  "clientype": "Personal",
+  "_class": "com.example.personal.entity.Personal"
+},{
+  "_id": 3,
+  "name": "Jhonatan",
+  "lastname": "Evanan Mendoza",
+  "email": "jevanan@gmail.com",
+  "dni": "23703254",
+  "phone": 916559357,
+  "clientype": "Personal",
+  "_class": "com.example.personal.entity.Personal"
+},{
+  "_id": 4,
+  "name": "Frank",
+  "lastname": "Cortez Neyra",
+  "email": "fcortez@gmail.com",
+  "dni": "61462124",
+  "phone": 916793424,
+  "clientype": "Personal",
+  "_class": "com.example.personal.entity.Personal"
+},{
+  "_id": 5,
+  "name": "Edinson",
+  "lastname": "Carbajal De La Cruz",
+  "email": "ecarbajal@gmail.com",
+  "dni": "20356225",
+  "phone": 802471257,
+  "clientype": "Personal",
+  "_class": "com.example.personal.entity.Personal"
+},{
+  "_id": 6,
+  "name": "Ivan",
+  "lastname": "Lapa Mateu",
+  "email": "ilapa@gmail.com",
+  "dni": "82598351",
+  "phone": 924943574,
+  "clientype": "Personal",
+  "_class": "com.example.personal.entity.Personal"
+}
+]
+);
